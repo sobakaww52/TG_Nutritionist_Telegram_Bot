5b67f46b9038e73f2e61dcabfc3e0989c85fba81
gleb_PC <glebuslav@mail.ru>
2026-04-05 21:15:01 +0300

    сделал сессию(транзакции), добавил ф-ию для создания таблиц

--- app/database.py
+++ app/database.py
@@ -6,3 +6,11 @@ from app.config import settings
 
 engine = create_async_engine(settings.database_url, echo=True, future=True)
 
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
