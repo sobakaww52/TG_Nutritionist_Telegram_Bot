2f4804685cbc76cc7214ad699a7d1640a5e5b7f2
gleb_laptop <glebuslav@mail.ru>
2026-05-07 03:52:09 +0300

    Deploy

--- app/config.py
+++ app/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     
     @property
     def database_url(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@localhost:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     model_config = SettingsConfigDict(env_file=".env")
 
